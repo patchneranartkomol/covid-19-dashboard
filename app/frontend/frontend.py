f96eab40958af9aed9b7947d54f9a85f653f7e21
Patch Neranartkomol <patch@neranartkomol.com>
2020-03-21 23:12:54 -0700

    Add global case count

--- app/frontend/frontend.py
+++ app/frontend/frontend.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 
+from ..utils.jhu_data import get_country_data, get_global_cases
+
 
 frontend_bp = Blueprint('frontend', __name__, template_folder='templates')
 
@@ -10,3 +12,11 @@ def index():
     Main page of the application
     """
     return render_template('main.html')
+
+@frontend_bp.context_processor
+def data_processor():
+    """
+    Merge country data into request context 
+    """
+    return dict(country_data = get_country_data(),
+                global_data = get_global_cases())
--- app/utils/jhu_data.py
+++ app/utils/jhu_data.py
@@ -1,5 +1,5 @@
-import csv
 from collections import defaultdict
+from csv import DictReader
 from datetime import datetime, timedelta
 
 import requests
@@ -25,29 +25,58 @@ def get_country_data(days_back=1):
     dict[dict]:
         JHU totals by country
             {
-                'Country' : str,
-                'Confirmed' : int,
-                'Deaths' : int,
-                'Recovered' : int
+                'country' : str :
+                {
+                    'confirmed' : int,
+                    'deaths' : int,
+                    'recovered' : int
+                }
             }
     """
     country_totals = defaultdict(dict)
     day = (datetime.today() - timedelta(days_back)).strftime('%m-%d-%Y')
     request = requests.get(JHU_URL.format(day=day))
 
-    data = csv.DictReader(request.text.splitlines())
+    data = DictReader(request.text.splitlines())
     for row in data:
         country = row.get('Country/Region', '')
         confirmed = int(row.get('Confirmed', 0))
         deaths = int(row.get('Deaths', 0))
         recovered = int(row.get('Recovered', 0))
+        last_updated = row.get('Last Update', '')
 
         if country not in country_totals:
-            country_totals[country]['Confirmed'] = confirmed
-            country_totals[country]['Deaths'] = deaths
-            country_totals[country]['Recovered'] = recovered
+            country_totals[country]['confirmed'] = confirmed
+            country_totals[country]['deaths'] = deaths
+            country_totals[country]['recovered'] = recovered
+            country_totals[country]['last_updated'] = last_updated
         else:
-            country_totals[country]['Confirmed'] += confirmed
-            country_totals[country]['Deaths'] += deaths
-            country_totals[country]['Recovered'] += recovered
+            country_totals[country]['confirmed'] += confirmed
+            country_totals[country]['deaths'] += deaths
+            country_totals[country]['recovered'] += recovered
     return country_totals
+
+def get_global_cases(days_back=1):
+    """
+    Gets global number of cases from JHU data
+
+    Parameters
+    ----------
+    days_back: int
+        Number of days from today to look back
+
+    Returns
+    -------
+    dict:
+        {
+            'confirmed' : int,
+            'deaths' : int,
+            'recovered' : int
+        }
+    """
+    global_cases = {}
+    data = get_country_data()
+    global_cases['confirmed'] = sum(c['confirmed'] for c in data.values() if c)
+    global_cases['deaths'] = sum(c['deaths'] for c in data.values() if c)
+    global_cases['recovered'] = sum(c['recovered'] for c in data.values() if c)
+    return global_cases
